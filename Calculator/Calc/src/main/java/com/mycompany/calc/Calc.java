e475e11800aefbd54bd1cd9359e47e8fbf39b5e8
khaled hany <tetandro2@gmail.com>
2025-07-02 13:42:21 +0300

    calcv3

--- Calculator/Calc/src/main/java/com/mycompany/calc/Calc.java
+++ Calculator/Calc/src/main/java/com/mycompany/calc/Calc.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class Calc {
     static double Sum (double n1,double n2){
-        System.out.println("The result is :"+n1+n2);
+        System.out.println("The result is :"+(n1+n2));
        return n1+n2; 
     }
     static double Sub (double n1,double n2){
     
-        System.out.println("The result is :"+n1-n2);
+        System.out.println("The result is :"+(n1-n2));
     
     return n1-n2;
     }
     static double div (double n1,double n2){
         
-            System.out.println("The result is :"+n1/n2);
+            System.out.println("The result is :"+(n1/n2));
     return n1/n2;
     }
     static double mul (double n1,double n2){
     
-        System.out.println("The result is :"+n1*n2);
+        System.out.println("The result is :"+(n1*n2));
         
     return n1*n2;
     }
    
     public static void main(String[] args) {
         
-        
+        System.out.println("------------------------ Welcome to calculator App Made by khaled ------------------------");
         
         
     }
