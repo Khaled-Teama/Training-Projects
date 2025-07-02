c4826eeb7208b7c232914466d08edee6e744a27d
khaled hany <tetandro2@gmail.com>
2025-07-02 13:37:55 +0300

    calv2

--- Calculator/Calc/src/main/java/com/mycompany/calc/Calc.java
+++ Calculator/Calc/src/main/java/com/mycompany/calc/Calc.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Calc {
     static double Sum (double n1,double n2){
-        System.out.println(n1+n2);
+        System.out.println("The result is :"+n1+n2);
        return n1+n2; 
     }
     static double Sub (double n1,double n2){
     
-        System.out.println(n1-n2);
+        System.out.println("The result is :"+n1-n2);
     
     return n1-n2;
     }
     static double div (double n1,double n2){
         
-            System.out.println(n1/n2);
+            System.out.println("The result is :"+n1/n2);
     return n1/n2;
     }
     static double mul (double n1,double n2){
